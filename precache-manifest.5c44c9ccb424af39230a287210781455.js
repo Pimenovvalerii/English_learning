8254e6926ee280a24fe69788c0b0eb5539ad6af0
Pimenovvalerii <Pimenovvalerii92@gmail.com>
2020-01-22 21:56:41 +0200

    Updates

---
+++ precache-manifest.5c44c9ccb424af39230a287210781455.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b34e6febe4507ea760e38dc430db1a",
+    "url": "/English_learning/index.html"
+  },
+  {
+    "revision": "828eba79594ab0fc5cc9",
+    "url": "/English_learning/static/css/main.9ff33d97.chunk.css"
+  },
+  {
+    "revision": "ea458cfeb4feb677e236",
+    "url": "/English_learning/static/js/2.7015441a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/English_learning/static/js/2.7015441a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "828eba79594ab0fc5cc9",
+    "url": "/English_learning/static/js/main.302a7aca.chunk.js"
+  },
+  {
+    "revision": "63936fb3ac0dca026f74",
+    "url": "/English_learning/static/js/runtime-main.e7c05f9f.js"
+  },
+  {
+    "revision": "0300b18f2542dcfe8cfbbeb54b04b340",
+    "url": "/English_learning/static/media/500_F_163345647_zRVgMg4IuSOcUS34d8dUpKb7yhlMF0I5.0300b18f.jpg"
+  },
+  {
+    "revision": "3131ad457fef255701bf06a612f57221",
+    "url": "/English_learning/static/media/словарь.3131ad45.jpeg"
+  }
+]);
